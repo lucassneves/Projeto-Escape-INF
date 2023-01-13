7de3a128b3e30a13d0073cf6ddb69ce189eb30a2
FDaniela <056daniela@gmail.com>
2023-01-13 16:52:03 -0300

    Adding SergioOffice to PlantaBaixa

--- Escape INF/Rooms/PlantaBaixa/PlantaBaixa.gd
+++ Escape INF/Rooms/PlantaBaixa/PlantaBaixa.gd
@@ -1,5 +1,11 @@
 extends Control
 
+onready var button1 = $Button
+onready var button2 = $Button2
+
+
 func _on_Button_pressed():
-	
-	var _a = get_tree().change_scene("res://Rooms/Lab257/Lab257.tscn")
+	if button1.is_pressed():
+		var _a = get_tree().change_scene("res://Rooms/Lab257/Lab257.tscn")
+	if button2.is_pressed():
+		var _b = get_tree().change_scene("res://Rooms/SergioOficina/Oficina.tscn")
