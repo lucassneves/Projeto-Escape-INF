4784ca6feddb1a55913c659b8a5b3a36e979dbdb
lucassneves <lucass.neves17@gmail.com>
2023-01-20 17:37:55 -0300

    novas salas e planta baixa

--- Escape INF/Rooms/PlantaBaixa/PlantaBaixa.gd
+++ Escape INF/Rooms/PlantaBaixa/PlantaBaixa.gd
@@ -1,14 +1,27 @@
 extends Control
 
-onready var button1 = $Button
-onready var button2 = $Button2
-onready var button3 = $Button3
+onready var lab257 = $lab257
+onready var lab152 = $lab152
+onready var lab251 = $lab251
+onready var salaSergio = $salaSergio
+onready var NRC = $NRC
+onready var copa = $copa
+onready var secretaria = $secretaria
 
 
 func _on_Button_pressed():
-	if button1.is_pressed():
+	if lab257.is_pressed():
 		var _a = get_tree().change_scene("res://Rooms/Lab257/Lab257.tscn")
-	if button2.is_pressed():
+	if salaSergio.is_pressed():
 		var _b = get_tree().change_scene("res://Rooms/SalaSergio/Sergio.tscn")
-	if button3.is_pressed():
+	if NRC.is_pressed():
 		var _c = get_tree().change_scene("res://Rooms/SalaNRC/SalaNRC.tscn")
+	if lab152.is_pressed():
+		var _d = get_tree().change_scene("res://Rooms/Lab152/Lab257.tscn")
+	if lab251.is_pressed():
+		var _e = get_tree().change_scene("res://Rooms/Lab251/Lab251.tscn")
+	if copa.is_pressed():
+		var _f = get_tree().change_scene("res://Rooms/Copa/Copa.tscn")
+	if secretaria.is_pressed():
+		var _g = get_tree().change_scene("res://Rooms/Secretaria/Secretaria.tscn")
+
