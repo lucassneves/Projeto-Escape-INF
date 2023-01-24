d72ca082a7b6d9b85717df1a0051f4aee182ba42
Angelo <41450351+Angelinis@users.noreply.github.com>
2023-01-24 14:56:39 -0300

    Adding an item inside Minibox

--- Escape INF/Interactables/LockedItems/CoffeeMachine/CoffeeMachineContainer.gd
+++ Escape INF/Interactables/LockedItems/CoffeeMachine/CoffeeMachineContainer.gd
@@ -7,6 +7,6 @@ onready var panels = get_children()
 #onready var destinyButton = $TextureRect8
 
 func _ready():
-		TextBox.show_texts(["O café está muito bom, agora posso continuar o desafio!"])
+		TextBox.show_texts(["Que simbolos mais estranhos para se colocar num copo"])
 	#get_parent().complete()
 		
--- Escape INF/Interactables/Puzzles/BoxOpener/BoxOpenedContainer.gd
+++ Escape INF/Interactables/Puzzles/BoxOpener/BoxOpenedContainer.gd
@@ -7,6 +7,6 @@ onready var panels = get_children()
 #onready var destinyButton = $TextureRect8
 
 func _ready():
-		TextBox.show_texts(["Este copo está estranho. Parece ter um grabado..."])
+		TextBox.show_texts(["Uma folha de papel. Deve ter algo importante..."])
 	#get_parent().complete()
 		
