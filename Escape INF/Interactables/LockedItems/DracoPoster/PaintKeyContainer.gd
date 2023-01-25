fce8a89c6645a906721d70e52f6e3399f4deff9d
Angelo <41450351+Angelinis@users.noreply.github.com>
2023-01-25 15:46:34 -0300

    Adding Item to 251 Lab

---
+++ Escape INF/Interactables/LockedItems/DracoPoster/PaintKeyContainer.gd
@@ -0,0 +1,12 @@
+extends Control
+
+var completed = false
+
+onready var panels = get_children()
+
+#onready var destinyButton = $TextureRect8
+
+func _ready():
+		TextBox.show_texts(["A chave do laboratorio!!"])
+	#get_parent().complete()
+		
