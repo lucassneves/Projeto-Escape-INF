260ac92233f2410b3490a8e13ce0dc86046235e2
Adrm00 <andreribeiromiranda5000@gmail.com>
2023-02-09 22:44:24 -0300

    Estilos de interface

--- Escape INF/Interface/InventoryRect/InventoryRect.gd
+++ Escape INF/Interface/InventoryRect/InventoryRect.gd
@@ -1,10 +1,14 @@
 extends Control
 
+const ITEM_PANEL = preload("res://Interface/InventoryRect/itemPanel.tres")
+const SELECTED_ITEM_PANEL = preload("res://Interface/InventoryRect/selectedItemPanel.tres")
+
 var hovering_slot_index = null
 var selected_slot_index = null setget select_slot
 
+var _hovering = false
+
 onready var slotsGrid := $HBoxContainer/GridContainer
-onready var selectedRect := $TextureRect
 onready var itemDescription := $HBoxContainer/ItemDescription
 
 signal selected_item_changed
@@ -29,6 +33,7 @@ func slot_mouse_exited():
 
 func _input(event):
 	if event is InputEventMouseButton:
+		
 		if event.button_index == BUTTON_LEFT and event.pressed and hovering_slot_index != null:
 			var item: Item = Inventory.items[hovering_slot_index]
 			if item != null:
@@ -36,20 +41,25 @@ func _input(event):
 					self.selected_slot_index = null # Deselect
 				else:
 					self.selected_slot_index = hovering_slot_index # Select
-				
+	
+	if event is InputEventMouseButton and event.button_index == BUTTON_LEFT and event.pressed and get_parent().visible and not _hovering:
+		get_parent().visible = false
+		get_tree().set_input_as_handled()
 
 func select_slot(slot_index):
 	
+	var old_selected_slot_index = selected_slot_index
 	selected_slot_index = slot_index
 	Inventory.selected_item_index = selected_slot_index
 	emit_signal("selected_item_changed")
 	
+	if old_selected_slot_index != null:
+		slotsGrid.get_child(old_selected_slot_index).set("custom_styles/panel", ITEM_PANEL)
+	
 	if selected_slot_index == null:
-		selectedRect.hide()
 		itemDescription.update_selected_item(null)
 	else:
-		selectedRect.show()
-		selectedRect.rect_position = slotsGrid.get_child(slot_index).rect_position
+		slotsGrid.get_child(selected_slot_index).set("custom_styles/panel", SELECTED_ITEM_PANEL)
 		itemDescription.update_selected_item(Inventory.items[slot_index])
 	
 func update_slot(slot_index):
@@ -66,11 +76,13 @@ func update_slot(slot_index):
 		slotsGrid.get_child(slot_index).get_child(0).queue_free()
 		self.selected_slot_index = null
 
-func _on_Button_pressed():
-	get_parent().hide()
-
 func _on_InventoryRect_gui_input(event):
 	if event is InputEventMouseButton:
 		if event.pressed and event.button_index == BUTTON_LEFT:
 			get_parent().show()
-			
+
+func _on_InventoryRect_mouse_entered():
+	_hovering = true
+
+func _on_InventoryRect_mouse_exited():
+	_hovering = false
