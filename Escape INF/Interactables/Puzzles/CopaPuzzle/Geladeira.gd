a8a544140612a0151dc95fe31e4e8d235904b55e
alphaonix <alphaonix@discente.ufg.br>
2023-02-12 21:13:08 -0300

    Colocando dica do puzzle do microondas

--- Escape INF/Interactables/Puzzles/CopaPuzzle/Geladeira.gd
+++ Escape INF/Interactables/Puzzles/CopaPuzzle/Geladeira.gd
@@ -1,9 +1,5 @@
 extends Control
 
-func _ready():
-		TextBox.show_texts(["Muito estranho esses números nas embalagens, será que queira me dizer algo???"])
-	#get_parent().complete()
-		
 func _on_TextureRect_gui_input(event):
 	if event is InputEventMouseButton and event.button_index == BUTTON_LEFT and event.pressed:
 		var _a = get_tree().change_scene(ProgressManager.previous_room)
