adae317d50572d0b6b8d466c9fb792b764b7e998
alphaonix <alphaonix@discente.ufg.br>
2023-02-14 00:21:11 -0300

    slide.gd

--- Escape INF/Rooms/Lab152/Wall0/Slide.gd
+++ Escape INF/Rooms/Lab152/Wall0/Slide.gd
@@ -1,10 +1,65 @@
 extends Area2D
 
+export(String) var locked_name
+export(bool) var locked := false
 
+export(Resource) var item_needed
+
+export(String, MULTILINE) var needed_text
+export(String, MULTILINE) var used_text
+
+export(String, FILE, "*.tscn") var goto
+
+onready var sprite = $Sprite
+onready var sprite2 = $Sprite2
+onready var sprite3 = $Sprite3
+
+var _hovering = false
+
+func _ready():
+	var room_file = get_tree().current_scene.filename
+	var wall_name = get_parent().name
+	
+	if ProgressManager.check_progress("unlocked_items", room_file, wall_name, locked_name):
+		locked = false
+		sprite.visible = true
+		sprite2.visible = true
+		sprite3.hide()
 
 func _on_Slide_mouse_entered():
-	pass # Replace with function body.
+	_hovering = true
 
 
 func _on_Slide_mouse_exited():
-	pass # Replace with function body.
+	_hovering = false
+	
+func _input(event):
+	if _hovering:
+		if event is InputEventMouseButton and event.button_index == BUTTON_LEFT and event.pressed:
+			interact()
+			get_tree().set_input_as_handled()
+		
+func interact():
+	
+	var room_file = get_tree().current_scene.filename
+	var wall_name = get_parent().name
+	var wall_index = get_parent().get_parent().current_wall_index
+	
+	if locked:
+		if Inventory.get_selected_item() == item_needed:
+			TextBox.show_texts(["Slide nostálgico, parece ter algo escrito nele que eu possa usar"])
+			locked = false
+			ProgressManager.add_unlocked_item(room_file, wall_name, locked_name)
+			Inventory.remove_item(item_needed)
+			sprite.visible = true
+			sprite2.visible = true
+			sprite3.hide()
+			ProgressManager.anxiety -= 10
+		else:
+			TextBox.show_texts(["Preciso de algo para colocar aqui!"])
+	else:
+		if goto:
+			ProgressManager.previous_room = room_file
+			ProgressManager.previous_wall_name = wall_name
+			ProgressManager.previous_wall_index = wall_index
+			var _a = get_tree().change_scene(goto)
